867a81a0570f93e89d033abcc99bf71d1b73d9fa
dnaccarati <davide.naccarati@uk.dk.com>
2023-03-26 16:57:42 +0100

    added first component

---
+++ src/components/Button/button.stories.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { ComponentMeta, ComponentStory } from "@storybook/react";
+import Button from "./button";
+
+export default {
+  title: "Components/Button",
+  component: Button,
+} as ComponentMeta<typeof Button>;
+
+// Create a master template for mapping args to render the Button component
+const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />;
+
+// Reuse that template for creating different stories
+export const Primary = Template.bind({});
+Primary.args = { label: "Primary 😃", size: "large", type: "primary" };
+
+export const Secondary = Template.bind({});
+Secondary.args = { ...Primary.args, type: "secondary", label: "Secondary 😇" };
